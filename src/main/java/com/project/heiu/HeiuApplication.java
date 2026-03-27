e2070e6f6f8134a7e819337dbe16f4a8b23ddec1
Marcelo Araújo Silva <marceloaraujosilva739@gmail.com>
2026-03-27 12:47:35 -0300

    first commit

---
+++ src/main/java/com/project/heiu/HeiuApplication.java
@@ -0,0 +1,13 @@
+package com.project.heiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeiuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeiuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/heiu/HeiuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.heiu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeiuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
