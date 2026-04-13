74549bdbcac991d1db613996e681660d194b33c6
Marcelo Araújo Silva <marceloaraujosilva739@gmail.com>
2026-04-13 15:25:04 -0300

    finished the auth controller and improved the secret key

--- src/main/java/com/project/heiu/controller/AuthController.java
+++ src/main/java/com/project/heiu/controller/AuthController.java
@@ -1,6 +1,15 @@
 package com.project.heiu.controller;
 
+import com.project.heiu.domain.users.UserService;
+import com.project.heiu.domain.users.dto.LoginRequest;
+import com.project.heiu.domain.users.dto.LoginResponse;
+import com.project.heiu.domain.users.dto.UserRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> register(@RequestBody @Valid UserRequest request) {
+        userService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest request){
+        return ResponseEntity.ok(userService.login(request));
+    }
 
 }
